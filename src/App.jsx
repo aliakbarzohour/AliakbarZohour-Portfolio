d401bf7c148a1a6ca2dd33134a7017068b42b52a
aliakbarzohour <aliakbar.zohour@gmail.com>
2024-03-10 02:27:46 +0330

    basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
+// Preloader Component
+import { AnimatePresence } from "framer-motion";
+import Home from './pages/home/Home';
 
 function App() {
+  const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      window.scrollTo(0, 0);
+    }, 3000);
+  }, []);
+
   return (
     <>
-      hi
+      {/* Preloader */}
+      <AnimatePresence>
+        {/* {loading && <PreLoaderPage />} */}
+        {/* Routes */}
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<AboutPage />} />
+          <Route path="/works" element={<WorksPage />} />
+          <Route path="/techstack" element={<TechStackPage />} />
+          <Route path="/bookshelf" element={<BookShelfPage />} />
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/note/:id" element={<NotePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path='*' element={<NotFoundPage />} /> */}
+        </Routes>
+      </AnimatePresence>
     </>
   )
 }
